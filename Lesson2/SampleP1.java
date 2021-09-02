5b9669ba8fb7042c24e326c6a25860d7ffabdbd3
RIKO-tlov <bluebyrair@outlook.jp>
2021-09-02 22:27:47 +0900

    [finish]Lesson2 Quiz

---
+++ Lesson2/SampleP1.java
@@ -0,0 +1,17 @@
+//2-1
+class SampleP1 {
+  public static void main(String[] args){
+    System.out.print("ようこそJavaへ！");
+    System.out.print("Javaをはじめましょう！");
+  }
+}
+
+/*2-2 文字と数値を出力する
+class SampleP2 {
+  public static void main(String[] args) {
+    System.out.println('A');
+    System.out.println("ようこそJavaへ！");
+    System.out.println(123);
+  }
+}
+*/
---
+++ Lesson2/SampleP2.java
@@ -0,0 +1,17 @@
+//2-3
+class SampleP2 {
+  public static void main(String[] args) {
+    System.out.println(123);
+    System.out.println("\\100もらった");
+    System.out.println("またあした");
+    System.out.println("1\t2\t3");
+    //8進数
+    System.out.println(06);
+    System.out.println(024);
+    System.out.println(015);
+    //16進数
+    System.out.println(0x6);
+    System.out.println(0x14);
+    System.out.println(0xD);
+  }
+}
