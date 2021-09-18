8a2fa9429dbe69594fe004596d48aecf8361a980
RIKO-tlov <bluebyrair@outlook.jp>
2021-09-18 18:11:25 +0900

    [finish]Lesson4 Quiz

--- Lesson4/Sample2.java
+++ Lesson4/Sample2.java
@@ -5,7 +5,7 @@ class Sample2 {
     int sum = num1 + num2;
 
     System.out.println("変数num1の値は" + num1 + "です。");
-    System.out.println("変数num2の値は" + num2+ "です。");
+    System.out.println("変数num2の値は" + num2 + "です。");
     System.out.println("num1+num2の値は" + sum + "です。");
 
     num1 = num1 + 1;
---
+++ Lesson4/SampleP4.java
@@ -0,0 +1,16 @@
+//4-4
+import java.io.*;
+class SampleP4 {
+  public static void main(String[] args) throws IOException {
+    System.out.println("三角形の高さと底辺を入力してください。");
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String str1 = br.readLine();
+    String str2 = br.readLine();
+
+    int height = Integer.parseInt(str1);
+    int width = Integer.parseInt(str2);
+
+    System.out.println("三角形の面積は" + ((height*width) / (double)2) + "です。");
+  }
+}
