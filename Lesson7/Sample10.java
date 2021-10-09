128dd65fb1f5a800f48fb9321b0c381d55e64ae7
RIKO-tlov <bluebyrair@outlook.jp>
2021-10-09 23:41:34 +0900

    [add]Lesson7

--- Lesson7/Sample10.java
+++ Lesson7/Sample10.java
@@ -16,7 +16,7 @@ class Sample10 {
 
     for(int i=0; i<5; i++) {
       System.out.println((i+1) + "番目の人の国語の点数は" + test[0] [i] + "です。");
-      System.out.println((i+1) + "番目の人の数学の点数は" + test[1] [i] + "です");
+      System.out.println((i+1) + "番目の人の数学の点数は" + test[1] [i] + "です。");
     }
   }
 }
