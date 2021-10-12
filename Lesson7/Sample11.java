8c99f602694721f0f528876d8c878cd5feba618d
RIKO-tlov <bluebyrair@outlook.jp>
2021-10-12 23:33:38 +0900

    [add]Lesson7 多次元配列

--- Lesson7/Sample11.java
+++ Lesson7/Sample11.java
@@ -1,22 +1,11 @@
 class Sample11 {
   public static void main(String[] args) {
-    int[] [] test;
-    test = new int[2] [5];
+    int[] [] test = {
+      {80, 60, 22, 50}, {90, 55, 68, 72}, {33, 75, 65}
+    };
 
-    test[0] [0] = 80;
-    test[0] [1] = 60;
-    test[0] [2] = 22;
-    test[0] [3] = 50;
-    test[0] [4] = 75;
-    test[1] [0] = 90;
-    test[1] [1] = 55;
-    test[1] [2] = 68;
-    test[1] [3] = 72;
-    test[1] [4] = 58;
-
-    for(int i=0; i<5; i++) {
-      System.out.println((i+1) + "番目の人の国語の点数は" + test[0] [i] + "です。");
-      System.out.println((i+1) + "番目の人の数学の点数は" + test[1] [i] + "です");
+    for(int i=0; i<test.length; i++) {
+      System.out.println((i+1) + "番目の配列要素の長さは" + test[i].length + "です。");
     }
   }
 }
